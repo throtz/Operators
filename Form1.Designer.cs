baa6749017f7dc5b85eee5db3626844c982b0996
Luka Kerovec <lazydrotz@hotmail.com>
2021-04-03 20:51:19 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace Operators
+{
+    partial class Operators
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCheck = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // buttonCheck
+            // 
+            this.buttonCheck.Location = new System.Drawing.Point(139, 41);
+            this.buttonCheck.Name = "buttonCheck";
+            this.buttonCheck.Size = new System.Drawing.Size(75, 23);
+            this.buttonCheck.TabIndex = 0;
+            this.buttonCheck.Text = "Check";
+            this.buttonCheck.UseVisualStyleBackColor = true;
+            this.buttonCheck.Click += new System.EventHandler(this.ButtonCheck_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(245, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.TabIndex = 2;
+            // 
+            // comboBox
+            // 
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Location = new System.Drawing.Point(118, 12);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(121, 23);
+            this.comboBox.TabIndex = 3;
+            // 
+            // Operators
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 76);
+            this.Controls.Add(this.comboBox);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonCheck);
+            this.Name = "Operators";
+            this.Text = "Operators";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCheck;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Operators
+{
+    public partial class Operators : Form
+    {
+        public Operators()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            comboBox.Items.AddRange(new string[] { "+", "-", "*", "/", "%", ">", "<", "=", ">=", "<=" });
+        }
+        private void ButtonCheck_Click_1(object sender, EventArgs e)
+        {
+            if (sender is null)
+            {
+                throw new ArgumentNullException(nameof(sender));
+            }
+
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+
+            Nah(a, b);
+        }
+        private void Nah(int a, int b)
+        {
+            switch (comboBox.SelectedItem.ToString())
+            {
+                case "+":
+                    MessageBox.Show("Sum of thenumbers entered is: " + (a + b));
+                    break;
+                case "-":
+                    MessageBox.Show("Subtraction of the numbers entered is: " + (a - b));
+                    break;
+                case "*":
+                    MessageBox.Show("Multiplication of the numbers entered is: " + (a * b));
+                    break;
+                case "/":
+                    MessageBox.Show("Division of the numbers entered is: " + (a / b));
+                    break;
+                case "%":
+                    MessageBox.Show("Modulo the two numbers is: " + (a % b));
+                    break;
+                case ">":
+                    MessageBox.Show("First number is bigger than the secound one " + (a > b));
+                    break;
+                case "<":
+                    MessageBox.Show("First number is smaller than the secound one " + (a < b));
+                    break;
+                case "=":
+                    MessageBox.Show("Numbers are equal: " + (a == b));
+                    break;
+                case ">=":
+                    MessageBox.Show("First number is bigger or equal to number two " + (a >= b));
+                    break;
+                case "<=":
+                    MessageBox.Show("First number is smaller or equal to number two " + (a <= b));
+                    break;
+                default:
+                    MessageBox.Show("Illegal operation.");
+                    break;
+            }
+        }
+    }
+}
